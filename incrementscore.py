ad5bef8f3a17f3fc47e4a27532f05ed0f645d5ca
Don Mills <dmills@singlestoneconsulting.com>
2016-02-29 13:57:23 -0500

    Added error check to incrementscore

--- incrementscore.py
+++ incrementscore.py
@@ -5,15 +5,19 @@
 import boto3
 import json
 def lambda_handler(event, context):
+    teams = ["A","B","C"]
     dynamo = boto3.resource('dynamodb').Table("testscore")
-    response = dynamo.update_item(
-    Key={
-        'team' : event['team']
-    },
-    UpdateExpression="set score = score + :val",
-    ExpressionAttributeValues={
-        ':val': 1
-    }
-) 
-    retval = "Added one to team " + event["team"]
+    if event['team'] in teams:
+        response = dynamo.update_item(
+        Key={
+            'team' : event['team']
+            },
+        UpdateExpression="set score = score + :val",
+        ExpressionAttributeValues={
+            ':val': 1
+            }
+        )
+        retval = "Added one to team " + event["team"]
+    else:
+        retval = "No such team - must be A, B, or C"
     return retval
